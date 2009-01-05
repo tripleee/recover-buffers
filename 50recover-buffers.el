e013b2faf4bb86230aea1dbd50a2f7caa8879421
era eriksson <era+emacs@iki.fi>
2009-01-05 18:43:00 +0200

    50recover-buffers.el: Debian-style startup file for recover-buffers
    darcs-hash:20090105164300-ca19f-5fdf9c3353d630c35caa0042a3da74080a5f6d36.gz

---
+++ 50recover-buffers.el
@@ -0,0 +1,6 @@
+;;; 50recover-buffers.el --- debian-style startup file for recover-buffers
+;; era Mon Jan  5 18:42:25 2009
+
+(autoload 'recover-buffers "recover-buffers" nil t)
+
+;;; 50recover-buffers.el ends here
